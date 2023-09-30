f6cda3f904dda14cf362c894691073a016ff0ffd
themashcodee <codeemash@gmail.com>
2023-09-30 20:01:44 +0530

    refactor: :hammer: minor change in emoji formating

--- src/utils/emojis.ts
+++ src/utils/emojis.ts
@@ -1,14 +1,20 @@
-import { emojify } from "node-emoji"
+import { emojify } from "node-emoji";
 
 const missing_emojis: {
-	[key: string]: string
+  [key: string]: string;
 } = {
-	"heart_hands": "🫶",
-	"white_frowning_face": "🙁",
-}
+  heart_hands: "🫶",
+  white_frowning_face: "🙁",
+};
 
 export const parseEmojis = (text: string) => {
-	return emojify(text, {
-		fallback: (name) => missing_emojis[name] ?? `:${name}:`,
-	})
-}
+  let emojified = text;
+
+  for (const [key, value] of Object.entries(missing_emojis)) {
+    emojified = emojified.replace(key, value);
+  }
+
+  emojified = emojify(emojified);
+
+  return emojified;
+};
