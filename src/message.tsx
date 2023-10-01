f5e0f1460fabb61d6640caf1f43f16492a43e75e
themashcodee <codeemash@gmail.com>
2023-10-01 14:17:51 +0530

    feat: :sparkles: setup tailwindcss n add build script

--- src/message.tsx
+++ src/message.tsx
@@ -1,34 +1,35 @@
-import { Block } from "./types"
-import { Header } from "./header"
-import { getBlockComponent } from "./components"
-import { BlockWrapper } from "./block-wrapper"
+import { Block } from "./types";
+import { Header } from "./header";
+import { getBlockComponent } from "./components";
+import { BlockWrapper } from "./block-wrapper";
+import "./global.css";
 
 type Props = {
-	theme?: "light" | "dark"
-	blocks: Block[]
-	logo: string
-	name: string
-	time?: Date
-}
+  theme?: "light" | "dark";
+  blocks: Block[];
+  logo: string;
+  name: string;
+  time?: Date;
+};
 
 export const Message = (props: Props) => {
-	const { theme = "light", blocks, logo, name, time = new Date() } = props
+  const { theme = "light", blocks, logo, name, time = new Date() } = props;
 
-	return (
-		<section className="flex gap-2 w-full max-w-[600px] slack-message">
-			<div className="shrink-0">
-				<img src={logo} className="w-9 h-9" alt={name} />
-			</div>
-			<div className="flex flex-col w-full">
-				<Header name={name} time={time} />
+  return (
+    <section className="flex gap-2 w-full max-w-[600px] slack-message">
+      <div className="shrink-0">
+        <img src={logo} className="w-9 h-9" alt={name} />
+      </div>
+      <div className="flex flex-col w-full">
+        <Header name={name} time={time} />
 
-				{blocks.map((block, i) => {
-					const element = getBlockComponent(block)
-					if (!element) return null
+        {blocks.map((block, i) => {
+          const element = getBlockComponent(block);
+          if (!element) return null;
 
-					return <BlockWrapper key={i}>{element}</BlockWrapper>
-				})}
-			</div>
-		</section>
-	)
-}
+          return <BlockWrapper key={i}>{element}</BlockWrapper>;
+        })}
+      </div>
+    </section>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				white: {
+					primary: "#ffffff",
+					"secondary": "#f8f8f8",
+				},
+				"custom_shadow": {
+					"1": "rgba(29, 28, 29, 0.13) 0px 0px 0px 1px, rgba(0, 0, 0, 0.12) 0px 5px 10px 0px",
+				},
+				black: {
+					primary: "rgb(29, 28, 29)",
+					"primary.3": "rgba(29, 28, 29, 0.3)",
+					secondary: "rgb(97, 96, 97)",
+				},
+				gray: {
+					primary: "rgb(221,221,221)",
+				},
+				red: {
+					primary: " rgb(224, 30, 90)",
+					"primary.3": "rgba(224, 30, 90, 0.3)",
+				},
+				blue: {
+					primary: "rgb(18,100,163)",
+				},
+				green: {
+					primary: "rgb(0, 122, 90)",
+					"primary.3": "rgba(0, 122, 90, 0.3)",
+				},
+			},
+			fontSize: {
+				"small": ["13px", "1.38461538"],
+				"base": ["15px", "1.46668"],
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
+}
