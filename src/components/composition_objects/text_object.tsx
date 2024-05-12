f85e040a0f87c08f10955a9880b068d8fa181618
themashcodee <codeemash@gmail.com>
2024-05-12 18:55:48 +0530

    feat: ✨ replace markdown parser

--- src/components/composition_objects/text_object.tsx
+++ src/components/composition_objects/text_object.tsx
@@ -12,8 +12,11 @@ export const TextObject = (props: TextObjectProps) => {
 
   // TODO: HANDLE VERBATIM
 
-  const emoji_parsed =
+  let emoji_parsed =
     type == "mrkdwn" ? parseEmojis(text) : emoji === false ? text : parseEmojis(text);
+
+  // BASIC MARKDOWN PARSING
+  emoji_parsed = emoji_parsed.replace(/&gt;/g, "> ").replace(/&lt;/g, "<");
 
   if (type === "plain_text")
     return <div className={className}>{slack_text_to_jsx(emoji_parsed, { markdown: false })}</div>;
--- src/utils/slack_text_to_jsx.ts
+++ src/utils/slack_text_to_jsx.ts
@@ -1,6 +1,9 @@
 import { ReactNode } from "react";
 import parse from "html-react-parser";
-import { slack_text_parser } from "./slack_text_parser";
+// import { slack_text_parser } from "./slack_text_parser";
+import { toHTML } from "slack-markdown";
+
+const slack_text_parser = toHTML;
 
 type Options = {
   markdown: boolean;
@@ -8,6 +11,12 @@ type Options = {
 
 export const slack_text_to_jsx = (text: string, parse_options: Options): ReactNode => {
   if (!text) return null;
-  const html_string = slack_text_parser(text, { markdown: parse_options.markdown });
+  const html_string = slack_text_parser(text, {
+    slackCallbacks: {
+      user(data) {
+        return `<span class="slack_user" data-user-id="${data.id}">@Manish Panwar</span>`;
+      },
+    },
+  });
   return parse(html_string, { trim: false });
 };
