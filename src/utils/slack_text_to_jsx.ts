4b72ecd1952dccfcd1500817a00bfab586bc3cde
themashcodee <codeemash@gmail.com>
2024-05-22 18:07:40 +0530

    chore: 🧼 remove unused package

--- src/utils/slack_text_to_jsx.ts
+++ src/utils/slack_text_to_jsx.ts
@@ -1,10 +1,7 @@
 import { ReactNode } from "react";
 import parse from "html-react-parser";
-// import { slack_text_parser } from "./slack_text_parser";
-import { toHTML } from "slack-markdown";
+import { toHTML as slack_text_parser } from "slack-markdown";
 import { GlobalStore } from "../store";
-
-const slack_text_parser = toHTML;
 
 type Options = {
   markdown: boolean;
@@ -21,25 +18,28 @@ export const slack_text_to_jsx = (text: string, parse_options: Options): ReactNo
   const html_string = slack_text_parser(text, {
     slackCallbacks: {
       user(data) {
-        if (hooks.user) return hooks.user(data);
-
         const user = users.find((u) => u.id === data.id || u.name === data.name);
+
+        if (hooks.user) return hooks.user(user || data);
+
         const label = user?.name || data.id || data.name;
         return `<span class="slack_user" data-user-id="${user?.id || data.id}">@${label}</span>`;
       },
       channel(data) {
-        if (hooks.channel) return hooks.channel(data);
-
         const channel = channels.find((c) => c.id === data.id || c.name === data.name);
+
+        if (hooks.channel) return hooks.channel(channel || data);
+
         const label = channel?.name || data.id || data.name;
         return `<span class="slack_channel" data-channel-id="${
           channel?.id || data.id
         }">#${label}</span>`;
       },
       atChannel(data) {
-        if (hooks.atChannel) return hooks.atChannel(data);
-
         const channel = channels.find((c) => c.name === data.name);
+
+        if (hooks.atChannel) return hooks.atChannel(channel || data);
+
         const label = channel?.name || data.name;
         return `<span class="slack_channel" data-channel-id="${channel?.id}">#${label}</span>`;
       },
