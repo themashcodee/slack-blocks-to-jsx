641bd4be2144fa088f28e72c461bdd8eb25274a2
themashcodee <codeemash@gmail.com>
2024-07-12 21:37:37 +0530

    fix: 🐛 wrap slack emoji by span

--- src/utils/markdown_parser/sub_elements/slack_emoji.tsx
+++ src/utils/markdown_parser/sub_elements/slack_emoji.tsx
@@ -10,7 +10,12 @@ export const SlackEmoji = (props: Props) => {
   const { element } = props;
   const { hooks } = useGlobalData();
 
-  if (hooks.emoji) return hooks.emoji(element.value, (name) => parseEmojis(`:${name}:`));
+  if (hooks.emoji)
+    return (
+      <span className="slack_emoji">
+        {hooks.emoji(element.value, (name) => parseEmojis(`:${name}:`))}
+      </span>
+    );
 
   return <span className="slack_emoji">{parseEmojis(`:${element.value}:`)}</span>;
 };
