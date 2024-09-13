95e792fa4a5fe458ce5cc22ac70e2ca832934274
themashcodee <codeemash@gmail.com>
2024-09-14 01:50:13 +0530

    fix: 🐛 fix slack channel parsing when name is given

--- src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
+++ src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
@@ -11,9 +11,20 @@ export const SlackChannelMention = (props: Props) => {
 
   const channel_id = element.value;
   const channel = channels.find((u) => u.id === channel_id || u.name === channel_id);
-  const label = channel?.name || channel_id;
+  const label = channel?.name || channel_id.split("|")[1] || channel_id;
 
-  if (hooks.channel) return <>{hooks.channel(channel || { id: channel_id, name: label })}</>;
+  if (hooks.channel) {
+    return (
+      <>
+        {hooks.channel(
+          channel || {
+            id: channel_id.split("|")[0] || channel_id,
+            name: label,
+          },
+        )}
+      </>
+    );
+  }
 
   return (
     <span className="slack_channel" data-channel-id={channel?.id || channel_id}>
