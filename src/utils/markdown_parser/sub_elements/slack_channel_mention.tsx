c699403e49a70c09b3d59e6b48e860e73b2edc50
themashcodee <codeemash@gmail.com>
2025-01-30 19:52:17 +0530

    fix: 🐛 fix given channel id matching when <#C1TEST|> syntax is passed

--- src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
+++ src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
@@ -10,7 +10,9 @@ export const SlackChannelMention = (props: Props) => {
   const { hooks, channels } = useGlobalData();
 
   const channel_id = element.value;
-  const channel = channels.find((u) => u.id === channel_id || u.name === channel_id);
+  const channel = channels.find(
+    (u) => u.id === channel_id.split("|")[0] || u.name === channel_id.split("|")[0],
+  );
   const label = channel?.name || channel_id.split("|")[1] || channel_id.split("|")[0] || channel_id;
 
   if (hooks.channel) {
