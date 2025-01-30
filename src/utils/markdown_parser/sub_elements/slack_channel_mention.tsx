7ecee427814ae501bc2d4b869ad40ee157b4a5cf
themashcodee <codeemash@gmail.com>
2025-01-30 19:41:45 +0530

    fix: 🐛 fix slack channel parsing when <#C1TEST|> syntax is passed

--- src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
+++ src/utils/markdown_parser/sub_elements/slack_channel_mention.tsx
@@ -11,7 +11,7 @@ export const SlackChannelMention = (props: Props) => {
 
   const channel_id = element.value;
   const channel = channels.find((u) => u.id === channel_id || u.name === channel_id);
-  const label = channel?.name || channel_id.split("|")[1] || channel_id;
+  const label = channel?.name || channel_id.split("|")[1] || channel_id.split("|")[0] || channel_id;
 
   if (hooks.channel) {
     return (
