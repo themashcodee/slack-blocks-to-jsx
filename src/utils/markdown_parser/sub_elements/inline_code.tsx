959127916e4e6684b2446e0f35995054ea7254c0
Tyler Perin <perintyler@gmail.com>
2025-12-23 16:54:53 -0500

    support links inside code blocks

--- src/utils/markdown_parser/sub_elements/inline_code.tsx
+++ src/utils/markdown_parser/sub_elements/inline_code.tsx
@@ -4,8 +4,32 @@ type Props = {
   element: InlineCodeSubElement;
 };
 
+function extractUrl(value: string): string | null {
+  const markdownLinkMatch = value.match(/^\[([^\]]+)\]\(([^)]+)\)$/);
+  if (markdownLinkMatch && markdownLinkMatch[1] === markdownLinkMatch[2]) {
+    return markdownLinkMatch[2] as string;
+  }
+
+  const slackLinkMatch = value.match(/^<(https?:\/\/[^>]+)>$/);
+  if (slackLinkMatch) return slackLinkMatch[1] as string;
+
+  const bareUrlMatch = value.match(/^(https?:\/\/\S+)$/);
+  if (bareUrlMatch) return bareUrlMatch[1] as string;
+
+  return null;
+}
+
 export const InlineCode = (props: Props) => {
   const { element } = props;
+  const url = extractUrl(element.value);
+
+  if (url) {
+    return (
+      <a href={url} className="slack_code_inline slack_link">
+        {url}
+      </a>
+    );
+  }
 
   return <code className="slack_code_inline">{element.value}</code>;
 };
